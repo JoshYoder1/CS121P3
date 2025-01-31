2f922b1a5fd21fe1928afab4fcfeb14144a507e0
JoshYoder1 <josh.yoder@bsu.edu>
2025-01-31 15:46:22 +0000

    update Horseracing.cpp

--- HorseRacing.cpp
+++ HorseRacing.cpp
@@ -6,29 +6,33 @@ void printLane(int, int*);
 bool isWinner(int,int*);
 
 int main(){
-	int horses[] = [0, 0, 0, 0, 0]
+	int horses[] = {0, 0, 0, 0, 0};
 	bool KeepGoing = true;
 //	printLane();
-	while(KeepGoing){
-		for(int i = 0, i<5, i++){
+	while(bool KeepGoing = true){
+		for(int i = 0; i<5; i++){
 			advance(i, horses);
 			printLane(i, horses);
 			if(isWinner(i, horses)){
-				keepGoing = false
+				KeepGoing = false;
 			} // end if
 		} // end for
 	} // end while
 } // end main	
 
 void advance(int horseNum, int* horses){
-	for(int i = 0, i < 5, i++){
+	for(int i = 0; i < 5; i++){
 		std::random_device rd;
-		std::uniform_int_distributuion<int> dist(0,1);
-		int coin = dist(Rd);
+		std::uniform_int_distribution<int> dist(0,1);
+		int coin = dist(rd);
 		if(coin == 1){
-			*horses[i] = horses[i] + 1;
+			horses[i] = horses[i] + 1;
 		} // end if
 	} // end for
 } // end advance
 
-void printLane(int horseNum, int* horses
+void printLane(int horseNum, int* horses){}
+
+bool isWinner(int,int*){
+	return true;
+}
